149b5ccd2617162be6143559ca47d084295613fb
Vanshshah2325 <vanshshah2325@gmail.com>
2026-01-04 19:49:14 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from '../components/Particle'
-import pdf from "../assets/vansh_s_Resume.pdf"
+import pdf from "../assets/Vansh_Shah_Resume .pdf";
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
